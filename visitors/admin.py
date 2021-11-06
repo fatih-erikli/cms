205b4a0f27acb2517d944486e98a9fb5705f600e
Fatih Erikli <fatih-erikli@outlook.com>
2021-11-06 15:42:12 +0300

    Natural time filter in admin

--- visitors/admin.py
+++ visitors/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from visitors.models import Visitor
 
+from django.contrib.humanize.templatetags.humanize import naturaltime
+
 class VisitorAdmin(admin.ModelAdmin):
     ordering = ('-date_latest_activity',)
-    list_display = ('ip_address', 'date_creation', 'date_latest_activity', 'browser_name', 'refresh_count')
+    list_display = ('ip_address', 'date_creation', 'latest_activity', 'browser_name', 'refresh_count')
+
+    def latest_activity(self, object):
+        return naturaltime(object.date_latest_activity)
 
 admin.site.register(Visitor, VisitorAdmin)
